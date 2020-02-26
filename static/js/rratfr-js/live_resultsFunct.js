40a7e082061d010a2063edbb5ac5b7174428e579
rak3rman <rak3rman@gmail.com>
2020-02-25 18:00:21 -0600

    Main page updates

--- static/js/rratfr-js/live_resultsFunct.js
+++ static/js/rratfr-js/live_resultsFunct.js
@@ -27,51 +27,57 @@ let tableSettings = {
         "emptyTable": "Entries will be updated soon!"
     },
 };
-let results2018 = $('#results2018').DataTable(
-    {
-        "lengthMenu": [
-            [10, 25, 50, -1],
-            [10, 25, 50, "All"]
-        ],
-        "responsive": true,
-        "ordering": false,
-        "searching": false,
-        "bLengthChange": false
-    }
-);
-let leaderTable = $('#leaderTable').DataTable(tableSettings);
+let pastSettings = {
+    "lengthMenu": [
+        [10, 25, 50, -1],
+        [10, 25, 50, "All"]
+    ],
+    "responsive": true,
+    "order": false,
+    "columnDefs": [
+        { "orderable": false, "targets": 0 }
+    ],
+    "language": {
+        "emptyTable": "Entries will be updated soon!"
+    },
+};
+//let leaderTable = $('#leaderTable').DataTable(tableSettings);
+let results2019 = $('#results2019').DataTable(pastSettings);
+let results2018 = $('#results2018').DataTable(pastSettings);
+let results2017 = $('#results2017').DataTable(pastSettings);
+let results2016 = $('#results2016').DataTable(pastSettings);
 $(window).trigger('resize');
 //Socket.io Get Statistics
-socket.on('race_data', function (data) {
-    document.getElementById("totalStat").innerHTML = data.total_entries;
-    document.getElementById("inwaterStat").innerHTML = data.entries_in_water;
-    document.getElementById("finishStat").innerHTML = data.entries_finished;
-    document.getElementById("pushedDate1").innerHTML = 'Updated ' + moment(data.updated_total_entries).fromNow();
-    document.getElementById("pushedDate2").innerHTML = 'Updated ' + moment(data.updated_entries_in_water).fromNow();
-    document.getElementById("pushedDate3").innerHTML = 'Updated ' + moment(data.updated_entries_finished).fromNow();
-    document.getElementById("votes_cast").innerHTML = data.votes_cast.toString() + ' Votes Cast';
-});
+// socket.on('race_data', function (data) {
+//     document.getElementById("totalStat").innerHTML = data.total_entries;
+//     document.getElementById("inwaterStat").innerHTML = data.entries_in_water;
+//     document.getElementById("finishStat").innerHTML = data.entries_finished;
+//     document.getElementById("pushedDate1").innerHTML = 'Updated ' + moment(data.updated_total_entries).fromNow();
+//     document.getElementById("pushedDate2").innerHTML = 'Updated ' + moment(data.updated_entries_in_water).fromNow();
+//     document.getElementById("pushedDate3").innerHTML = 'Updated ' + moment(data.updated_entries_finished).fromNow();
+//     document.getElementById("votes_cast").innerHTML = data.votes_cast.toString() + ' Votes Cast';
+// });
 
 //Socket.io Get Leaderboard Data
-socket.on('entry_data', function (data) {
-    leaderTable.clear();
-    $.each(data, function (i, value) {
-        console.log(value.final_place + ". " + value.entry_name +  " " + value.final_time + value.category);
-        let final_place_text = value.final_place;
-        if (value.final_place === "1") {
-            final_place_text = "<strong><a style='color:#D4AF37'>" + value.final_place + "</a></strong>";
-        }
-        if (value.final_place === "2") {
-            final_place_text = "<strong><a style='color:#C4CACE'>" + value.final_place + "</a></strong>";
-        }
-        if (value.final_place === "3") {
-            final_place_text = "<strong><a style='color:#CD7F32'>" + value.final_place + "</a></strong>";
-        }
-        let detailed_name = value.entry_name + " <a class='text-gray'>" + value.bib_number + "</a>";
-        leaderTable.row.add([final_place_text, detailed_name, value.final_time, value.category]);
-    });
-    leaderTable.draw();
-});
+// socket.on('entry_data', function (data) {
+//     leaderTable.clear();
+//     $.each(data, function (i, value) {
+//         console.log(value.final_place + ". " + value.entry_name +  " " + value.final_time + value.category);
+//         let final_place_text = value.final_place;
+//         if (value.final_place === "1") {
+//             final_place_text = "<strong><a style='color:#D4AF37'>" + value.final_place + "</a></strong>";
+//         }
+//         if (value.final_place === "2") {
+//             final_place_text = "<strong><a style='color:#C4CACE'>" + value.final_place + "</a></strong>";
+//         }
+//         if (value.final_place === "3") {
+//             final_place_text = "<strong><a style='color:#CD7F32'>" + value.final_place + "</a></strong>";
+//         }
+//         let detailed_name = value.entry_name + " <a class='text-gray'>" + value.bib_number + "</a>";
+//         leaderTable.row.add([final_place_text, detailed_name, value.final_time, value.category]);
+//     });
+//     leaderTable.draw();
+// });
 
 //Select Dashboard Image
 function setImage() {
